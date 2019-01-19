c28353d8bbd96ca12301417dbf582f7990b7a1f4
Kevin James Stevens <kevstev.realty@gmail.com>
2019-01-19 19:49:09 +0800

    update 1.0002

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+/**
+* Filename: main.js
+* Developed by: Kevin James G. Stevens
+* Last Updated: January 2019
+* Purpose: [Script]
+*/
+
+(function($){
+	
+	'use strict';
+	alert("Sample Alert!");
+
+})(jQuery);
+
