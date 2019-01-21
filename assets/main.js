81f11fbdc1abed1dee6c9a544e1951d8374ecb8b
Kevin James Stevens <kevstev.realty@gmail.com>
2019-01-21 18:31:38 +0800

    update 1.005

--- assets/main.js
+++ assets/main.js
@@ -9,17 +9,23 @@
 	
 	'use strict';
 
+	// Page Link Scrolling Animation
 	$('.page-link').click(function() {
 		var anchor = $(this).attr("dest");
 		// $('.link-wrap').removeClass('visible');
-
 		// $('nav span').removeClass('active');
 		// $("nav").find('[dest="'+ anchor +'"]').addClass('active');
-
 		$('html, body').animate({
 		  scrollTop: $('#' + anchor).offset().top
 		}, 600);
   	});
+
+  	// Slide Animation Handler
+	$(window).scroll(function (event) {
+		var scroll = $(window).scrollTop();
+		console.log(scroll);
+	});
+  	
 
 })(jQuery);
 
